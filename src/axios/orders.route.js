3ead177ae4b22af0f9208913a1938c5b1078528f
= <=>
2022-08-05 21:26:15 +0530

    changes

--- src/axios/orders.route.js
+++ src/axios/orders.route.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = 'http://localhost:8080/api';
-const BASE_URL = 'http://185.193.125.115:8080/api';
+const BASE_URL = 'http://localhost:8080/api';
 
 export const ordersGet = async (status, user_id) =>
     await axios.post(`${BASE_URL}/orders`, { status, user_id })
