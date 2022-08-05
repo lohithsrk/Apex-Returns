b1f5daa60558702555ee476f969d177dda6ceb61
= <=>
2022-08-05 20:18:12 +0530

    changes

--- src/axios/investments.axios.js
+++ src/axios/investments.axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : 'http://apexreturns.com/';
+const BASE_URL = 'http://apexreturns.com/';
 
 export const getInvestments = async (user_id) =>
     await axios.get(`${BASE_URL}/investment/${user_id}`)
--- src/axios/payment.axios.js
+++ src/axios/payment.axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080/api' : 'http://apexreturns.com/';
+const BASE_URL = 'http://apexreturns.com/';
 
 export const createDeposite = async (user_id, amount, reference_id,) =>
     await axios.post(`${BASE_URL}/deposite/apex/payment`, { user_id, amount, reference_id, })
