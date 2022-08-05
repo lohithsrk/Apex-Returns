c00cdb3ad5bde45d44692b6c622e7842c3029719
= <=>
2022-08-05 21:21:48 +0530

    changes

--- src/axios/investments.axios.js
+++ src/axios/investments.axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const BASE_URL = 'http://localhost:8080/api';
-const BASE_URL = 'http://185.193.125.115/api';
+const BASE_URL = 'http://185.193.125.115:8080/api';
 
 export const getInvestments = async (user_id) =>
     await axios.get(`${BASE_URL}/investment/${user_id}`)
