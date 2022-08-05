ecb4949cf9e1c45f24b8e32f70d9e2af6a202d09
= <=>
2022-08-05 21:20:23 +0530

    changes

--- src/axios/orders.route.js
+++ src/axios/orders.route.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080/api';
-const BASE_URL = 'http://apexreturns.com/api';
+const BASE_URL = 'http://185.193.125.115/api';
 
 export const ordersGet = async (status, user_id) =>
     await axios.post(`${BASE_URL}/orders`, { status, user_id })
--- src/axios/payment.axios.js
+++ src/axios/payment.axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://localhost:8080/api';
-const BASE_URL = 'http://apexreturns.com/api';
+const BASE_URL = 'http://185.193.125.115/api';
 
 export const createDeposite = async (user_id, amount, reference_id,) =>
     await axios.post(`${BASE_URL}/deposite/apex/payment`, { user_id, amount, reference_id, })
