1b63acb4b0d10cc8346a157a2f706db613883d6f
= <=>
2022-08-12 13:08:25 +0530

    changes

--- src/utils/common.util.js
+++ src/utils/common.util.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080/api';
-// export const BASE_URL = 'https://apexreturns.com/api';
+// export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'https://apexreturns.com/api';
 
