606ca30af331612702b99b6bd61dcaa5c1dc42bd
= <=>
2022-08-13 18:04:16 +0530

    changes

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -23,7 +23,7 @@ exports.UIPGet = async (req, res) => {
 }
 
 exports.withdrawRequestsGet = async (req, res) => {
-    await db.query('SELECT * FROM withdraw WHERE aproved = "Pending"', (err, result) => {
+    await db.query('SELECT * FROM withdraw WHERE approved = "Pending"', (err, result) => {
         if (err) {
             console.log(err);
             res.status(500).send('Error');
@@ -34,4 +34,50 @@ exports.withdrawRequestsGet = async (req, res) => {
 }
 
 exports.withdrawRequestsPost = async (req, res) => {
+    console.log(req.body);
+    await db.query('UPDATE withdraw SET approved = ? WHERE id = ?', [req.body.approved, req.body.id], async (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send('Error');
+        } else {
+            console.log(result);
+            await db.query('UPDATE user SET amount = amount - ? WHERE id = ?', [req.body.amount, req.body.user_id], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return res.status(500).send('Error');
+                } else {
+                    return res.send('Withdraw request approved');
+                }
+            })
+        }
+    })
+}
+
+exports.depositVerificationGet = async (req, res) => {
+    await db.query('SELECT * from deposit WHERE verification = "pending"', (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error');
+        } else {
+            res.json(result);
+        }
+    })
+}
+
+exports.depositVerificationPost = async (req, res) => {
+    await db.query('UPDATE deposit SET verification = ? WHERE id = ?', [req.body.verification, req.body.id], async (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error');
+        } else {
+            await db.query('UPDATE user SET amount = amount + ? WHERE id = ?', [req.body.amount, req.body.user_id], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send('Error');
+                } else {
+                    res.send('Deposit verified');
+                }
+            })
+        }
+    })
 }
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { changeUPI, UIPGet, withdrawRequestsGet, withdrawRequestsPost } = require('../controllers/admin.controller');
+const { changeUPI, UIPGet, withdrawRequestsGet, withdrawRequestsPost, depositVerificationGet, depositVerificationPost } = require('../controllers/admin.controller');
 
 router.route('/admin/changeUPI').post(changeUPI).get(UIPGet)
 router.route('/admin/withdraw').post(withdrawRequestsPost).get(withdrawRequestsGet)
+router.route('/admin/deposit').post(depositVerificationPost).get(depositVerificationGet)
 
 module.exports = router;
