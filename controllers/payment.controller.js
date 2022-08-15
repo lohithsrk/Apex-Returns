0459a0a45990ef45177663490c9e1a7982bf3c58
= <=>
2022-08-15 18:46:39 +0530

    chanhe

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -1,21 +1,51 @@
 const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
 
 const db = require('../database')
 
 exports.paymentPost = async (req, res) => {
 
-    const { user_id, amount, reference_id } = req.body;
-    await db.query('INSERT INTO deposit SET id = ?, user_id = ?, reference_id = ?, amount = ?, verification = ?, created_at = ?', [uuidv4(), user_id, reference_id, amount, 'pending', new Date()], (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({
-                message: 'Error',
-                error: err
-            });
-        }
-        return res.status(200).json({
-            message: 'Success',
-            data: result
+    const { user_id, amount } = req.body;
+    // await db.query('INSERT INTO deposit SET id = ?, user_id = ?, reference_id = ?, amount = ?, verification = ?, created_at = ?', [uuidv4(), user_id, reference_id, amount, 'pending', new Date()], (err, result) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.status(500).json({
+    //             message: 'Error',
+    //             error: err
+    //         });
+    //     }
+    //     return res.status(200).json({
+    //         message: 'Success',
+    //         data: result
+    //     });
+    // })
+
+    var data = JSON.stringify({
+        "key": `${process.env.PAYMENT_KEY}`,
+        "client_txn_id": uuidv4(),
+        "amount": `${amount}`,
+        "p_info": `${amount} APEX`,
+        "customer_name": `${user_id}`,
+        "customer_email": `${user_id}@gmail.com`,
+        "customer_mobile": `${user_id}`,
+        "redirect_url": "https://apexreturns.com",
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://merchant.upigateway.com/api/create_order',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            console.log(response.data);
+            res.json(response.data.data.payment_url);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
-    })
 }
