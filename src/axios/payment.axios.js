7d2175a80f4e65784a409c5ffc47f3e530f5e818
= <=>
2022-08-16 22:55:38 +0530

    changes

--- src/axios/payment.axios.js
+++ src/axios/payment.axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { BASE_URL } from '../utils/common.util';
 
 export const createDeposit = async (user_id, amount) =>
-    await axios.post(`${BASE_URL}/deposit/apex/payment`, { user_id, amount: 1 })
+    await axios.post(`${BASE_URL}/deposit/apex/payment`, { user_id, amount })
 
 export const verifyDeposit = async (client_txn_id, user_id) =>
     await axios.put(`${BASE_URL}/deposit/apex/payment`, {
