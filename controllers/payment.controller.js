5a6aa17511d466f10d01f823660749947d0aca8f
= <=>
2022-08-16 10:26:58 +0530

    changes

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -38,9 +38,9 @@ exports.paymentPost = async (req, res) => {
 }
 
 exports.createDepositBackup = async (req, res) => {
-    const { user_id, amount, reference_id } = req.body;
+    const { user_id, reference_id } = req.body;
 
-    await db.query('INSERT INTO deposit SET id = ?, user_id = ?, reference_id = ?, amount = ?, verification = ?, created_at = ?', [uuidv4(), user_id, reference_id, amount, 'pending', new Date()], (err, result) => {
+    await db.query('INSERT INTO deposit SET id = ?, user_id = ?, reference_id = ?, amount = 0, verification = ?, created_at = ?', [uuidv4(), user_id, reference_id, amount, 'pending', new Date()], (err, result) => {
         if (err) {
             console.log(err);
             return res.status(500).json({
