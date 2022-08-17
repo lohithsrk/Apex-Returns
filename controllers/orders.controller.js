8eb19d425257a7b5b4a935446babb7ed5e6efb88
= <=>
2022-08-17 22:06:59 +0530

    changes

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,12 +1,22 @@
 const db = require('../database');
 
 exports.ordersGet = async (req, res) => {
-    await db.query('SELECT * FROM deposit where verification = ? AND user_id = ?', [req.body.status, req.body.user_id], (err, rows) => {
+    await db.query('SELECT * FROM deposit where verification = ? AND user_id = ?', [req.body.status, req.body.user_id], async (err, rows) => {
         if (err) {
             res.status(500).send(err);
         }
         else {
-            res.json(rows);
+            await db.query('SELECT * FROM withdraw WHERE user_id = ?', [req.body.user_id], (err, rows1) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+                else {
+                    res.json({
+                        deposit: rows,
+                        withdraw: rows1
+                    });
+                }
+            })
         }
     })
 }
